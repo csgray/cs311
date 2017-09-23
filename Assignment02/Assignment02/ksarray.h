4f67dee420ab982e6448779c2fb3d96bdc65e4bf
Corey Gray <csgray@alaska.edu>
2017-09-23 15:45:07 -0800

    All tests passed!

--- Assignment02/Assignment02/ksarray.h
+++ Assignment02/Assignment02/ksarray.h
@@ -9,7 +9,7 @@
 #ifndef FILE_KSARRAY_H_INCLUDED
 #define FILE_KSARRAY_H_INCLUDED
 
-#include <algorithm> // For std::lexicographical_compare
+#include <algorithm> // For std::lexicographical_compare and std::swap
 
 // template KSArray
 // Desc
@@ -43,9 +43,8 @@ public:
 	}
 
 	// Move constructor
-	KSArray(KSArray && other) noexcept : _size(other._size)
+	KSArray(KSArray && other) noexcept : _size(other._size), _array(other._array)
 	{
-		_array = other._array;
 		other._size = 0;
 		other._array = NULL;
 	}
@@ -53,31 +52,15 @@ public:
 	// Copy assignment operator
 	KSArray & operator=(const KSArray & other)
 	{
-		if (&other != this)
-		{
-			if (_size != other._size)
-			{
-				_size = other._size;
-				delete[] _array;
-				_array = new ValType[_size];
-			}
-			for (auto i = 0; i != _size; ++i)
-				_array[i] = other._array[i];
-		}
+		KSArray copy(other);
+		mswap(copy);
 		return *this;
 	}
 
 	// Move assignment operator
 	KSArray & operator=(KSArray && other) noexcept
 	{
-		if (this != &other)
-		{
-			delete[] _array;
-			_size = other._size;
-			_array = other._array;
-			other._size = 0;
-			other._array = NULL;
-		}
+		mswap(other);
 		return *this;
 	}
 
@@ -91,7 +74,15 @@ private:
 	size_type _size;
 	value_type * _array;
 
-	// ***** KSArray: General public operators *****
+// ***** KSArray: Private functions ****
+private:
+	void mswap(KSArray & other) noexcept
+	{
+		std::swap(_size, other._size);
+		std::swap(_array, other._array);
+	}
+
+// ***** KSArray: General public operators *****
 public:
 
 	// operator[]
@@ -107,7 +98,7 @@ public:
 		return _array[i];
 	}
 
-	// ***** KSArray: General public member functions *****
+// ***** KSArray: General public member functions *****
 public:
 
 	// size
@@ -143,7 +134,6 @@ public:
 	{
 		return _array + _size;
 	}
-
 };
 
 // ***** class KSArray - Declaration of associated global operators *****
