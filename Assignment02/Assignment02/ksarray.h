38d20bc5c1c3cd8b07e9835524afeda266706c2e
Corey Gray <csgray@alaska.edu>
2017-09-23 14:05:50 -0800

    Almost all tests passed.

--- Assignment02/Assignment02/ksarray.h
+++ Assignment02/Assignment02/ksarray.h
@@ -9,6 +9,8 @@
 #ifndef FILE_KSARRAY_H_INCLUDED
 #define FILE_KSARRAY_H_INCLUDED
 
+#include <algorithm> // For std::lexicographical_compare
+
 // template KSArray
 // Desc
 // Invariants
@@ -21,17 +23,11 @@ public:
 	// Default constructor
 	// Pre: None
 	// Post
-	KSArray() : _size(10)
-	{
-		_array = new ValType [_size];
-	}
+	KSArray() : _size(10), _array(new ValType[_size]) {}
 
 	// 1-parameter constructor
 	// Pre: A non-negative integer giving the number of items in the array
-	KSArray(int number) : _size(number)
-	{
-		_array = new ValType[_size];
-	}
+	KSArray(int number) : _size(number), _array(new ValType[_size]) {}
 
 	// Destructor
 	~KSArray()
@@ -40,9 +36,8 @@ public:
 	}
 
 	// Copy constructor
-	KSArray(const KSArray & other) : _size(other._size)
+	KSArray(const KSArray & other) : _size(other._size), _array(new ValType[_size])
 	{
-		_array = new ValType[_size];
 		for (auto i = 0; i != _size; ++i)
 			_array[i] = other._array[i];
 	}
@@ -50,20 +45,39 @@ public:
 	// Move constructor
 	KSArray(KSArray && other) noexcept : _size(other._size)
 	{
-		_array = new ValType[_size];
-		for (auto i = 0; i != _size; ++i)
-			_array[i] = other._array[i];
+		_array = other._array;
+		other._size = 0;
+		other._array = NULL;
 	}
 
 	// Copy assignment operator
 	KSArray & operator=(const KSArray & other)
 	{
+		if (&other != this)
+		{
+			if (_size != other._size)
+			{
+				_size = other._size;
+				delete[] _array;
+				_array = new ValType[_size];
+			}
+			for (auto i = 0; i != _size; ++i)
+				_array[i] = other._array[i];
+		}
 		return *this;
 	}
 
 	// Move assignment operator
 	KSArray & operator=(KSArray && other) noexcept
 	{
+		if (this != &other)
+		{
+			delete[] _array;
+			_size = other._size;
+			_array = other._array;
+			other._size = 0;
+			other._array = NULL;
+		}
 		return *this;
 	}
 
@@ -77,7 +91,7 @@ private:
 	size_type _size;
 	value_type * _array;
 
-// ***** KSArray: General public operators *****
+	// ***** KSArray: General public operators *****
 public:
 
 	// operator[]
@@ -93,7 +107,7 @@ public:
 		return _array[i];
 	}
 
-// ***** KSArray: General public member functions *****
+	// ***** KSArray: General public member functions *****
 public:
 
 	// size
@@ -122,12 +136,12 @@ public:
 	// Post: Returns the address of the item one-past the end of the array
 	value_type * end()
 	{
-		return _array;
+		return _array + _size;
 	}
 
 	const value_type * end() const
 	{
-		return _array;
+		return _array + _size;
 	}
 
 };
@@ -144,7 +158,14 @@ public:
 template <typename value_type>
 bool operator==(const KSArray<value_type> & lhs, const KSArray<value_type> & rhs)
 {
-	return false;
+	if (lhs.size() != rhs.size())
+		return false;
+	for (auto i = 0; i != lhs.size(); ++i)
+	{
+		if (lhs[i] != rhs[i])
+			return false;
+	}
+	return true;
 }
 
 // inequality (!=)
@@ -157,35 +178,35 @@ bool operator==(const KSArray<value_type> & lhs, const KSArray<value_type> & rhs
 template <typename value_type>
 bool operator!=(const KSArray<value_type> & lhs, const KSArray<value_type> & rhs)
 {
-	return false;
+	return !(lhs == rhs);
 }
 
 // less than (<)
 template <typename ValType>
 bool operator<(const KSArray<ValType> & lhs, const KSArray<ValType> & rhs)
 {
-	return false;
+	return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
 }
 
 // less than or equal (<=)
 template <typename ValType>
 bool operator<=(const KSArray<ValType> & lhs, const KSArray<ValType> & rhs)
 {
-	return false;
+	return (lhs < rhs || lhs == rhs);
 }
 
 // greater than (>)
 template <typename ValType>
 bool operator>(const KSArray<ValType> & lhs, const KSArray<ValType> & rhs)
 {
-	return false;
+	return !(lhs < rhs || lhs == rhs);
 }
 
 // greater than or equal (>=)
 template <typename ValType>
 bool operator>=(const KSArray<ValType> & lhs, const KSArray<ValType> & rhs)
 {
-	return false;
+	return !(lhs < rhs);
 }
 
 #endif //#ifndef FILE_KSARRAY_H_INCLUDED
