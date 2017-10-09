6051dcee624df6a79f33e2cf36f967f3c78c7f94
Corey Gray <csgray@alaska.edu>
2017-10-08 19:05:05 -0800

    Add counthsr.h and counthsr.cpp. It compiles!

---
+++ Assignment04/Assignment04/counthsr.cpp
@@ -0,0 +1,96 @@
+// counthsr.cpp
+// Corey S. Gray
+// 08 Oct 2017
+//
+// For CS 311 Fall 2017
+// Source for Assignment 4
+
+#include "counthsr.h"
+#include <vector>
+using std::vector;
+
+// Nonrecursive wrapper function
+int countHSR(int dim_x, int dim_y,			// board dimensions
+			 int hole_x, int hole_y,		// hole coordinates
+			 int start_x, int start_y,		// start square coordinates
+			 int finish_x, int finish_y)	// finish square coordinates
+{
+	// Create an empty partial solution
+	vector<int> boardArray(dim_x * dim_y);
+	int pos_x = start_x;
+	int pos_y = start_y;
+	int squaresLeft = (dim_x * dim_y) - 2;
+
+	int total = 0;
+	// Call recursive workhorse with partial solution
+
+	// Return the return value of the workhorse function
+	return total;
+}
+
+// Recursive workhorse function
+int countHSR_recurse(vector<int> boardArray,
+					 int dim_x, int dim_y,
+					 int finish_x, int finish_y,
+					 int pos_x, int pos_y,
+					 int squaresLeft)
+{
+	// Do we have a full solution?
+	if (squaresLeft == 0 &&
+		pos_x == finish_x &&
+		pos_y == finish_y)
+			return 1;
+
+	int total = 0;
+
+	// Is column to left of current position on the board?
+	if (pos_x - 1 >= 0)
+	{
+		// Is row below current position on the board?
+		if (pos_y - 1 >= 0)
+		{
+
+		}
+
+		// Current row is on the board by default
+
+		// Is row above current position on the board?
+		if (pos_y + 1 <= dim_y)
+		{
+
+		}
+	}
+
+	// Current column is on the board by default
+		// Is row below current position on the board?
+		if (pos_y - 1 >= 0)
+		{
+
+		}
+
+		// Is row above current position on the board?
+		if (pos_y + 1 <= dim_y)
+		{
+
+		}
+
+	// Is column to right of current position on the board?
+	if (pos_x + 1 <= dim_x)
+	{
+		// Is row below current position on the board?
+		if (pos_y - 1 >= 0)
+		{
+
+		}
+
+		// Current row is on the board by default
+
+		// Is row above current position on the board?
+		if (pos_y + 1 <= dim_y)
+		{
+
+		}
+	}
+
+	return total;
+}
---
+++ Assignment04/Assignment04/counthsr.h
@@ -0,0 +1,24 @@
+// counthsr.h 
+// Corey S. Gray
+// 08 Oct 2017
+//
+// For CS 311 Fall 2017
+// Header for Assignment 4
+
+#ifndef FILE_COUNTHSR_H_INCLUDED
+#define FILE_COUNTHSR_H_INCLUDED
+
+#include <vector>
+
+int countHSR(int dim_x, int dim_y,			// board dimensions
+			 int hole_x, int hole_y,		// hole coordinates
+			 int start_x, int start_y,		// start square coordinates
+			 int finish_x, int finish_y);	// finish square coordinates
+
+int countHSR_recurse(std::vector<int> boardArray,
+					 int dim_x, int dim_y,
+					 int finish_x, int finish_y,
+					 int pos_x, int pos_y,
+					 int squaresLeft);
+
+#endif  //#ifndef FILE_COUNTHSR_H_INCLUDED
