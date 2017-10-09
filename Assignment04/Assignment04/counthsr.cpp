9f4807a4871a9e12b0cc8d6f51703f00394688df
Corey Gray <csgray@alaska.edu>
2017-10-08 21:09:20 -0800

    Update comments.

--- Assignment04/Assignment04/counthsr.cpp
+++ Assignment04/Assignment04/counthsr.cpp
@@ -3,60 +3,65 @@
 // 08 Oct 2017
 //
 // For CS 311 Fall 2017
-// Source for Assignment 4
+// Source for Assignment 4: countHSR recursive function
 
 #include "counthsr.h"
 #include <vector>
 using std::vector;
 
-// Nonrecursive wrapper function
-int countHSR(int dim_x, int dim_y,			// board dimensions
-			 int hole_x, int hole_y,		// hole coordinates
-			 int start_x, int start_y,		// start square coordinates
-			 int finish_x, int finish_y)	// finish square coordinates
+// countHSR
+// Documentation in header file.
+int countHSR(int dim_x, int dim_y,			// Board dimensions
+			 int hole_x, int hole_y,		// Hole coordinates
+			 int start_x, int start_y,		// Start square coordinates
+			 int finish_x, int finish_y)	// Finish square coordinates
 {
 	// Create an empty partial solution
 	vector<int> boardArray(dim_x * dim_y);
-	boardArray[start_x * dim_y + start_y] = 1; // Mark start as unavailable
-	boardArray[hole_x * dim_y + hole_y] = 1; // Mark hole as unavailable
-	int pos_x = start_x;
+	boardArray[start_x * dim_y + start_y] = 1;	// Mark start as unavailable
+	boardArray[hole_x * dim_y + hole_y] = 1;	// Mark hole as unavailable
+	int pos_x = start_x;						// Set current position to starting coordinates
 	int pos_y = start_y;
-	int squaresLeft = (dim_x * dim_y) - 2; // Total minus start and hole
+	int squaresLeft = (dim_x * dim_y) - 2;		// Total minus start and hole
 
+	// Start with zero solutions.
 	int total = 0;
 
-	// Call recursive workhorse with partial solution
+	// Call recursive workhorse with empty partial solution
 	total += countHSR_recurse(boardArray,
-		dim_x, dim_y,
-		finish_x, finish_y,
-		pos_x, pos_y,
-		squaresLeft);
+							  dim_x, dim_y,
+							  finish_x, finish_y,
+							  pos_x, pos_y,
+							  squaresLeft);
 
-	// Return the return value of the workhorse function
+	// Return the number of solutions.
 	return total;
 }
 
-// Recursive workhorse function
-int countHSR_recurse(vector<int> & boardArray,
-					 int dim_x, int dim_y,
-					 int finish_x, int finish_y,
-					 int pos_x, int pos_y,
-					 int squaresLeft)
+// countHSR_recurse
+// Documentation in header file.
+int countHSR_recurse(vector<int> & boardArray,		// Array of visited/unavailable squares
+					 int dim_x, int dim_y,			// Board dimensions
+					 int finish_x, int finish_y,	// Finish square coordinates
+					 int pos_x, int pos_y,			// Spider's current coordinates
+					 int squaresLeft)				// Number of unvisited squares
 {
 	// Do we have a full solution?
-	if (squaresLeft == 0 && pos_x == finish_x && pos_y == finish_y)
+	if (squaresLeft == 0 &&							// No more squares available
+		pos_x == finish_x && pos_y == finish_y)		// Current position is finish square
 		return 1;
 
+	// If not a full solution, set total to 0 and check for solutions.
 	int total = 0;
 
-	// Is column to left of current position on the board?
-	if (pos_x - 1 >= 0)
+	// Check for solutions left to right and bottom to top.
+	if (pos_x - 1 >= 0)	// Is column west of the current position on the board?
 	{
-		// Is row below current position on the board?
-		if (pos_y - 1 >= 0 &&
-			boardArray[(pos_x - 1) * dim_y + (pos_y - 1)] == 0) // Has this square been visited?
+		// Check for solutions to the SW.
+		if (pos_y - 1 >= 0 &&									// Is row below current position on the board?
+			boardArray[(pos_x - 1) * dim_y + (pos_y - 1)] == 0)	// Has this square not been visited?
 		{
-			// Update current position.
+			// Update current position to move SW.
 			--pos_x;
 			--pos_y;
 
@@ -73,17 +78,18 @@ int countHSR_recurse(vector<int> & boardArray,
 									  pos_x, pos_y,
 									  squaresLeft);
 
-			// Revert changes except updated total
+			// Revert changes except updated total.
 			++squaresLeft;
 			boardArray[pos_x * dim_y + pos_y] = 0;
 			++pos_x;
 			++pos_y;
 		}
 
-		// Current row is on the board by default
-		if (boardArray[(pos_x - 1) * dim_y + pos_y] == 0) // Has this square been visited?)
+		// Check for solutions to the W.
+		// Current row is on the board so no need to check that.
+		if (boardArray[(pos_x - 1) * dim_y + pos_y] == 0) // Has this square not been visited?
 		{
-			// Update current position.
+			// Update current position to move W.
 			--pos_x;
 
 			// Mark new square as visited.
@@ -99,17 +105,17 @@ int countHSR_recurse(vector<int> & boardArray,
 									  pos_x, pos_y,
 									  squaresLeft);
 
-			// Revert changes except updated total
+			// Revert changes except updated total.
 			++squaresLeft;
 			boardArray[pos_x * dim_y + pos_y] = 0;
 			++pos_x;
 		}
 
-		// Is row above current position on the board?
-		if (pos_y + 1 <= dim_y - 1 &&
-			boardArray[(pos_x - 1) * dim_y + (pos_y + 1)] == 0) // Has this square been visited?)
+		// Check for solutions to the NW.
+		if (pos_y + 1 <= dim_y - 1 &&							// Is row above current position on the board?
+			boardArray[(pos_x - 1) * dim_y + (pos_y + 1)] == 0) // Has this square not been visited?)
 		{
-			// Update current position.
+			// Update current position to move NW.
 			--pos_x;
 			++pos_y;
 
@@ -126,7 +132,7 @@ int countHSR_recurse(vector<int> & boardArray,
 									  pos_x, pos_y,
 									  squaresLeft);
 
-			// Revert changes except updated total
+			// Revert changes except updated total.
 			++squaresLeft;
 			boardArray[pos_x * dim_y + pos_y] = 0;
 			++pos_x;
@@ -134,13 +140,39 @@ int countHSR_recurse(vector<int> & boardArray,
 		}
 	}
 
-	// Current column is on the board by default
-	// Is row below current position on the board?
-	if (pos_y - 1 >= 0 &&
-		boardArray[pos_x * dim_y + (pos_y - 1)] == 0) // Has this square been visited?
+	// Current column is on the board so no need to check that.
+	// Check for solutions to the S.
+	if (pos_y - 1 >= 0 &&								// Is row below current position on the board?
+		boardArray[pos_x * dim_y + (pos_y - 1)] == 0)	// Has this square not been visited?
 	{
-		// Update current position.
+		// Update current position to move S.
 		--pos_y;
+
+		// Mark new square as visited.
+		boardArray[pos_x * dim_y + pos_y] = 1;
+
+		// Decrement number of squares left.
+		--squaresLeft;
+
+		// Make recursive call and add return value to total.
+		total += countHSR_recurse(boardArray,
+			dim_x, dim_y,
+			finish_x, finish_y,
+			pos_x, pos_y,
+			squaresLeft);
+
+		// Revert changes except updated total.
+		++squaresLeft;
+		boardArray[pos_x * dim_y + pos_y] = 0;
+		++pos_y;
+	}
+
+	// Check for solutions to the N.
+	if (pos_y + 1 <= dim_y - 1 &&						// Is row above current position on the board?
+		boardArray[pos_x * dim_y + (pos_y + 1)] == 0)	// Has this square been visited?
+	{
+		// Update current position to move N.
+		++pos_y;
 
 		// Mark new square as visited.
 		boardArray[pos_x * dim_y + pos_y] = 1;
@@ -158,43 +190,16 @@ int countHSR_recurse(vector<int> & boardArray,
 		// Revert changes except updated total
 		++squaresLeft;
 		boardArray[pos_x * dim_y + pos_y] = 0;
-		++pos_y;
-	}
-
-	// Is row above current position on the board?
-	if (pos_y + 1 <= dim_y - 1 &&
-		boardArray[pos_x * dim_y + (pos_y + 1)] == 0) // Has this square been visited?
-	{
-		// Update current position.
-		++pos_y;
-
-		// Mark new square as visited.
-		boardArray[pos_x * dim_y + pos_y] = 1;
-
-		// Decrement number of squares left.
-		--squaresLeft;
-
-		// Make recursive call and add return value to total.
-		total += countHSR_recurse(boardArray,
-			dim_x, dim_y,
-			finish_x, finish_y,
-			pos_x, pos_y,
-			squaresLeft);
-
-		// Revert changes except updated total
-		++squaresLeft;
-		boardArray[pos_x * dim_y + pos_y] = 0;
 		--pos_y;
 	}
 
-	// Is column to right of current position on the board?
-	if (pos_x + 1 <= dim_x - 1)
+	if (pos_x + 1 <= dim_x - 1)	// Is column east of current position on the board?
 	{
-		// Is row below current position on the board?
-		if (pos_y - 1 >= 0 &&
-			boardArray[(pos_x + 1) * dim_y + (pos_y - 1)] == 0) // Has this square been visited?
+		// Check for solutions to the SE.
+		if (pos_y - 1 >= 0 &&									// Is row below current position on the board?
+			boardArray[(pos_x + 1) * dim_y + (pos_y - 1)] == 0) // Has this square not been visited?
 		{
-			// Update current position.
+			// Update current position to move SE.
 			++pos_x;
 			--pos_y;
 
@@ -218,10 +223,11 @@ int countHSR_recurse(vector<int> & boardArray,
 			++pos_y;
 		}
 
-		// Current row is on the board by default
+		// Check for solutions to the E.
+		// Current row is on the board by default so no need to check that.
 		if (boardArray[(pos_x + 1) * dim_y + pos_y] == 0) // Has this square been visited?)
 		{
-			// Update current position.
+			// Update current position to move E.
 			++pos_x;
 
 			// Mark new square as visited.
@@ -243,11 +249,11 @@ int countHSR_recurse(vector<int> & boardArray,
 			--pos_x;
 		}
 
-		// Is row above current position on the board?
-		if (pos_y + 1 <= dim_y - 1 &&
+		// Check for solutions to the NE.
+		if (pos_y + 1 <= dim_y - 1 &&							// Is row above current position on the board?
 			boardArray[(pos_x + 1) * dim_y + (pos_y + 1)] == 0) // Has this square been visited?)
 		{
-			// Update current position.
+			// Update current position to move NE.
 			++pos_x;
 			++pos_y;
 
@@ -272,5 +278,6 @@ int countHSR_recurse(vector<int> & boardArray,
 		}
 	}
 
+	// Return total number of solutions found from current position.
 	return total;
 }
--- Assignment04/Assignment04/counthsr.h
+++ Assignment04/Assignment04/counthsr.h
@@ -3,18 +3,42 @@
 // 08 Oct 2017
 //
 // For CS 311 Fall 2017
-// Header for Assignment 4
+// Header for Assignment 4: countHSR recursive function
 
 #ifndef FILE_COUNTHSR_H_INCLUDED
 #define FILE_COUNTHSR_H_INCLUDED
 
 #include <vector>
 
+// countHSR
+// Counts the solutions to a "holey spider run" board.
+// Nonrecursive wrapper function that calls countHSR_recurse.
+// Preconditions:
+//		dim_x and dim_y > 0
+//		hole_x, start_x, and finish_x <= dim_x
+//		holy_y, start_y, and finish_y <= dim_y
+// Postconditions:
+//		Returns the total number of solutions as an integer.
+// Exceptions:
+//		Does not throw.
+// Implementation in source file.
 int countHSR(int dim_x, int dim_y,			// board dimensions
 			 int hole_x, int hole_y,		// hole coordinates
 			 int start_x, int start_y,		// start square coordinates
 			 int finish_x, int finish_y);	// finish square coordinates
 
+// countHSR_recurse
+// Recursive workhorse for countHSR.
+// Preconditions:
+//		dim_x and dim_y > 0
+//		hole_x, start_x, and finish_x <= dim_x
+//		holy_y, start_y, and finish_y <= dim_y
+//		squaresLeft  0;
+// Postconditions: 
+//		Returns the total number of solutions as an integer.
+// Exceptions:
+//		Does not throw.
+// Implementation in source file.
 int countHSR_recurse(std::vector<int> & boardArray,
 					 int dim_x, int dim_y,
 					 int finish_x, int finish_y,
