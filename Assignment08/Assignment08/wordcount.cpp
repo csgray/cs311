0a96cbd5554ed8c91159ddfe3bd014dd32c7c8cc
Corey Gray <csgray@alaska.edu>
2017-12-06 00:16:26 -0900

    Add input loop. Add comments.

--- Assignment08/Assignment08/wordcount.cpp
+++ Assignment08/Assignment08/wordcount.cpp
@@ -20,41 +20,41 @@ using std::map;
 int main()
 {
 	cout
-		<< "Word Count" << endl
+		<< "'Word Count' by C. S. Gray" << endl
 		<< "Enter a file name and this program will print the number of distinct words that appear in the file" << endl
 		<< "followed by a list of each individual word and the number of times it appears in the file." << endl
-		<< endl
-		<< "Type your filename: ";
+		<< endl;	
 
-	string filename;
-	getline(cin, filename);
-	ifstream input(filename);
-	if (!input)
+	// Global variables
+	string filename;			// User-specified file
+	ifstream input;				// Input stream for said file
+	map<string, int> counts;	// STL Table of <word, count> pairs
+	string word;				// Word read from file and added to map
+
+	// Repeatedly prompts the user for a filename until a file is successfully opened
+	while (true)
 	{
-		cout << "ERROR: Bad input!" << endl;
-		cout << "Please try again with a proper filename." << endl;
-		cout << "Press ENTER to quit.";
-		while (cin.get() != '\n');
-		return -1;
+		cout << "Type your filename: ";
+		getline(cin, filename);
+		input.open(filename);
+		if (input.is_open())	// only exits the loop if the file was successfully opened
+			break;
+		cout << "That cannot be opened. Please try again with another filename." << endl;
 	}
 
-	if (input.is_open())
-	{
-		cout << "Counting " << filename << "..." << endl << endl;
-		map<string, int> counts;
+	cout << "Counting '" << filename << "'..." << endl << endl;
+	// Adds each word to the map as a key or increments its value if already present
+	while (input >> word)
+		counts[word]++;
+	input.close();
 
-		string word;
-		while (input >> word)
-			counts[word]++;
-		input.close();
-
-		cout << "Number of distinct words: " << counts.size() << endl << endl;
-		for (const auto & kvpair : counts)
-			cout << kvpair.first << " " << kvpair.second << endl;
-	}
+	// Outputs the results by printing the map's size then iterating over it
+	cout << "Number of distinct words: " << counts.size() << endl << endl;
+	for (const auto & kvpair : counts)
+		cout << kvpair.first << " " << kvpair.second << endl;
 	
+	// Wait for user then exit
 	cout << endl << "Press ENTER to quit.";
 	while (cin.get() != '\n');
-
 	return 0;
 }
