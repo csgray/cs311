57664f65ef0d9e29fbb7ab50249f0af031920dfd
Corey Gray <csgray@alaska.edu>
2017-12-05 23:21:52 -0900

    Add wordcount.cpp as part of Assignment08

---
+++ Assignment08/Assignment08/wordcount.cpp
@@ -0,0 +1,57 @@
+// wordcount.cpp
+// Corey S. Gray
+// 05 Dec 2017
+//
+// For CS 311 Fall 2017
+// Source for Assignment 8: Word count using a STL table implementation
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <string>
+using std::string;
+using std::getline;
+#include <fstream>
+using std::ifstream;
+#include <map>
+using std::map;
+
+int main()
+{
+	cout
+		<< "Word Count" << endl
+		<< "Enter a file name and this program will print the number of distinct words that appear in the file" << endl
+		<< "followed by a list of each individual word and the number of times it appears in the file." << endl
+		<< endl
+		<< "Type your filename: ";
+
+	string filename;
+	getline(cin, filename);
+	ifstream input(filename);
+	if (!input)
+	{
+		cout << "ERROR: Bad input!" << endl;
+		cout << "Please try again with a proper filename." << endl;
+		cout << "Press ENTER to quit.";
+		while (cin.get() != '\n');
+		return -1;
+	}
+
+	if (input.is_open())
+	{
+		cout << "Counting " << filename << "..." << endl;
+		
+		string word;
+		while (input >> word)
+		{
+			cout << word << endl;
+		}
+		input.close();
+	}
+	
+	cout << "Press ENTER to quit.";
+	while (cin.get() != '\n');
+
+	return 0;
+}
